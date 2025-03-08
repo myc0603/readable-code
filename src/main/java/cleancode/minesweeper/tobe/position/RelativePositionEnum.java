42b7ba825274239b8a7002ddb3f7d751f9807a2a
Yechan Moon <moonyechan97@naver.com>
2025-03-09 00:09:48 +0900

    Value Object

---
+++ src/main/java/cleancode/minesweeper/tobe/position/RelativePositionEnum.java
@@ -0,0 +1,24 @@
+package cleancode.minesweeper.tobe.position;
+
+public enum RelativePositionEnum {
+
+    LEFT_UPPER(-1, -1), UPPER(-1, 0), RIGHT_UPPER(-1, 1),
+    LEFT(0, -1), RIGHT(0, 1),
+    LEFT_DOWN(1, -1), DOWN(0, -1), RIGHT_DOWN(1, 1);
+
+    private final int rowIndex;
+    private final int colIndex;
+
+    RelativePositionEnum(int rowIndex, int colIndex) {
+        this.rowIndex = rowIndex;
+        this.colIndex = colIndex;
+    }
+
+    public int getRowIndex() {
+        return rowIndex;
+    }
+
+    public int getColIndex() {
+        return colIndex;
+    }
+}
