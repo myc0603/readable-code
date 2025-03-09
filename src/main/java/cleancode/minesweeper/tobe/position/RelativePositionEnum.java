00a132c72f5328e0a2c5ef54bbd34a9733e2b2ab
Yechan Moon <moonyechan97@naver.com>
2025-03-09 20:56:39 +0900

    줄 조절

--- src/main/java/cleancode/minesweeper/tobe/position/RelativePositionEnum.java
+++ src/main/java/cleancode/minesweeper/tobe/position/RelativePositionEnum.java
@@ -2,9 +2,14 @@ package cleancode.minesweeper.tobe.position;
 
 public enum RelativePositionEnum {
 
-    LEFT_UPPER(-1, -1), UPPER(-1, 0), RIGHT_UPPER(-1, 1),
-    LEFT(0, -1), RIGHT(0, 1),
-    LEFT_DOWN(1, -1), DOWN(0, -1), RIGHT_DOWN(1, 1);
+    LEFT_UPPER(-1, -1),
+    UPPER(-1, 0),
+    RIGHT_UPPER(-1, 1),
+    LEFT(0, -1),
+    RIGHT(0, 1),
+    LEFT_DOWN(1, -1),
+    DOWN(0, -1),
+    RIGHT_DOWN(1, 1);
 
     private final int rowIndex;
     private final int colIndex;
