e0bdb19d744b0a8ce3a4f07e30d65d53afc3c955
Yechan Moon <moonyechan97@naver.com>
2025-03-11 17:20:41 +0900

    add private constructor of Displayer

--- src/main/java/cleancode/studycafe/chnn/io/Displayer.java
+++ src/main/java/cleancode/studycafe/chnn/io/Displayer.java
@@ -8,6 +8,9 @@ public class Displayer {
     private static final String displayFormatForHourly = "%s시간권 - %d원";
     private static final String displayFormatForWeekly = "%s주권 - %d원";
 
+    private Displayer() {
+    }
+
     public static String display(Pass pass) {
         String format = isPassHourly(pass) ? displayFormatForHourly : displayFormatForWeekly;
         return String.format(format, pass.getDuration(), pass.getPrice());
